b3afa933d2a19a034605c2729b62d99b4a7922fb
NasirHaidari <nasir.haidri@htomail.com>
2021-03-04 22:48:17 +0100

    edited configfile

--- src/config/config.js
+++ src/config/config.js
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://photoreviewapp.netlify.app/'
+  : 'https://photoreviewapp.netlify.app'
